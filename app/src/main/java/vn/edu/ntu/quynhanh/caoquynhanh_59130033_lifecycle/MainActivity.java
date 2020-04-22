fadecfb2e3c313b98a823ba55c9704860cefb871
Cao Quỳnh Anh <quynhanhcao99@gmail.com>
2020-04-22 08:00:07 +0700

    V 1.1

--- app/src/main/java/vn/edu/ntu/quynhanh/caoquynhanh_59130033_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/quynhanh/caoquynhanh_59130033_lifecycle/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LifeCycle", "onCreate called");
-        Toast.makeText(getApplicationContext(), ".....", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
         txtTime = findViewById(R.id.txtTime);
         SimpleDateFormat spf = new SimpleDateFormat("HH:mm:ss");
         String strDate = spf.format(new Date());
@@ -29,23 +29,42 @@ public class MainActivity extends AppCompatActivity {
     protected void  onStart(){
         super.onStart();
         Log.d("LifeCycle", "onStart called");
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void  onResume(){
         super.onResume();
         Log.d("LifeCycle", "onResume called");
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void  onPause(){
         super.onPause(();
         Log.d("LifeCycle", "onPause( called");
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void  onStop(){
         super.onStop();
         Log.d("LifeCycle", "onStop called");
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void  onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle", "onRestart called");
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void  onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle", "onDestroy called");
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
 }
