6c8c1326d7a34bcdae934651a0f5752d59a30d24
Cao Quỳnh Anh <quynhanhcao99@gmail.com>
2020-04-22 07:49:33 +0700

    V 1.1

--- app/src/main/java/vn/edu/ntu/quynhanh/caoquynhanh_59130033_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/quynhanh/caoquynhanh_59130033_lifecycle/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LifeCycle", "onCreate called");
+        Toast.makeText(getApplicationContext(), ".....", Toast.LENGTH_SHORT).show();
         txtTime = findViewById(R.id.txtTime);
         SimpleDateFormat spf = new SimpleDateFormat("HH:mm:ss");
         String strDate = spf.format(new Date());
