b672f0be1838b280c8bf6100d9ba9bcbcf09984f
Cao Quỳnh Anh <quynhanhcao99@gmail.com>
2020-04-22 07:12:23 +0700

    V 1.1

--- app/src/main/java/vn/edu/ntu/quynhanh/caoquynhanh_59130033_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/quynhanh/caoquynhanh_59130033_lifecycle/MainActivity.java
@@ -3,12 +3,47 @@ package vn.edu.ntu.quynhanh.caoquynhanh_59130033_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCycle", "onCreate called");
+        txtTime = findViewById(R.id.txtTime);
+        SimpleDateFormat spf = new SimpleDateFormat("HH:mm:ss");
+        String strDate = spf.format(new Date());
+        txtTime.setText(strDate);
+    }
+
+    @Override
+    protected void  onStart(){
+        super.onStart();
+        Log.d("LifeCycle", "onStart called");
+    }
+
+    @Override
+    protected void  onResume(){
+        super.onResume();
+        Log.d("LifeCycle", "onResume called");
+    }
+
+    @Override
+    protected void  onPause(){
+        super.onPause(();
+        Log.d("LifeCycle", "onPause( called");
+    }
+
+    @Override
+    protected void  onStop(){
+        super.onStop();
+        Log.d("LifeCycle", "onStop called");
     }
 }
